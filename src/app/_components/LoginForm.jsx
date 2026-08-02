933fe0d609aa675a8aee1f23197b3b3ba16c3db2
wol6 <alwynmathew007@gmail.com>
2026-08-03 03:08:03 +0530

    fix

--- src/app/_components/LoginForm.jsx
+++ src/app/_components/LoginForm.jsx
@@ -17,15 +17,15 @@ function LoginForm() {
             return { ...prev, [name]: value }
         })
     }
-   async function handleSignIn(e){
+    async function handleSignIn(e) {
         e.preventDefault()
-        try{
-            const {data:resp} = await API.post('/signin',loginObj)
-            if(resp.success){
-                sessionStorage.setItem('username',resp.name)
+        try {
+            const { data: resp } = await API.post('/signin', loginObj)
+            if (resp.success) {
+                sessionStorage.setItem('username', resp.user.name)
                 router.push('/dashboard')
             }
-        }catch(err){
+        } catch (err) {
             console.log(err)
         }
     }
@@ -34,7 +34,7 @@ function LoginForm() {
             <div className='bg-white px-8 py-13 w-full max-w-md rounded-2xl mb-20 border border-gray-200'>
                 <div className='text-center'>
                     <p className='text-2xl font-bold text-gray-700'>Welcome Back</p>
-                  <p className="text-sm text-gray-500 mt-2">Please enter your credential to sign in</p>
+                    <p className="text-sm text-gray-500 mt-2">Please enter your credential to sign in</p>
 
                 </div>
                 <div className='p-4 space-y-5'>
@@ -64,7 +64,7 @@ function LoginForm() {
                     >Sign In</button>
                 </div>
                 <p className="text-center text-sm text-gray-500">
-                     Don't have an account?{' '}
+                    Don't have an account?{' '}
                     <Link href={"/register"} className="font-semibold text-indigo-600 hover:text-indigo-500 hover:underline">
                         Sign up here
                     </Link>
